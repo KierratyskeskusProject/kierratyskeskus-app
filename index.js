2c909f4617ea7c0ea3fed4a5fc7e68817cd0622b
IO89 <osipov.ivan.89@gmail.com>
2018-08-30 13:25:33 +0300

    Showing port number

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+
+
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
-
-
-app.listen(PORT,() =>{
+app.listen(PORT, () => {
     console.log(`Server is up on ${PORT}`);
 });
