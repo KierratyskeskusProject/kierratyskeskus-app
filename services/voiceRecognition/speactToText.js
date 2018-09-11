a1c1eaf9faa01c880fe807de84e2d7dbb204d57e
danni <danni.r.olsen@gmail.com>
2018-09-11 10:59:08 +0300

    speach to text

--- services/voiceRecognition/speactToText.js
+++ services/voiceRecognition/speactToText.js
@@ -1,10 +1,10 @@
-//const auth = require('../config/.env');
+// const auth = require('../config/.env');
 const record = require('node-record-lpcm16');
 const speech = require('@google-cloud/speech');
 
 // Creates a client
 const client = new speech.SpeechClient({
-  keyFilename: __dirname+'/key.json'
+  keyFilename: `${__dirname}/key.json`,
 });
 
 
@@ -14,9 +14,9 @@ const languageCode = 'en-US';
 
 const request = {
   config: {
-    encoding: encoding,
-    sampleRateHertz: sampleRateHertz,
-    languageCode: languageCode,
+    encoding,
+    sampleRateHertz,
+    languageCode,
   },
   interimResults: true,
 };
@@ -25,18 +25,16 @@ const request = {
 const recognizeStream = client
   .streamingRecognize(request)
   .on('error', console.error)
-  .on('data', data =>
-    process.stdout.write(
-      data.results[0] && data.results[0].alternatives[0]
-        ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
-        : `\n\nReached transcription time limit, press Ctrl+C\n`
-    )
-  );
+  .on('data', data => process.stdout.write(
+    data.results[0] && data.results[0].alternatives[0]
+      ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
+      : '\n\nReached transcription time limit, press Ctrl+C\n',
+  ));
 
 // Start recording and send the microphone input to the Speech API
 record
   .start({
-    sampleRateHertz: sampleRateHertz,
+    sampleRateHertz,
     threshold: 0,
     verbose: false,
     recordProgram: 'rec',
@@ -46,4 +44,3 @@ record
   .pipe(recognizeStream);
 
 console.log('Listening, press Ctrl+C to stop.');
-
