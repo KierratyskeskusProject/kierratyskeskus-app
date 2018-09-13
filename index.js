12fc31a83aa2fdd27fe8d14716d47badbeaff00b
tarmeli <tommi.tarkki@gmail.com>
2018-09-13 18:10:21 +0300

    setup database in index.js

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 
+// open database in memory
+const db = new sqlite3.Database(':memory:', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to database');
+  return null;
+});
+
+// db.close((err) => {
+//   if (err) {
+//     return console.error(err.message);
+//   }
+//   console.log('Close the database connection.');
+//   return null;
+// });
 
 const app = express();
 
