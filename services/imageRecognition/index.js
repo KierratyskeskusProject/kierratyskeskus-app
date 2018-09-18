9d9c1124f2138e3fbf4a2108c0301b742ff2ceb9
tarmeli <tommi.tarkki@gmail.com>
2018-09-18 16:13:27 +0300

    script returns labels for the inputted image

--- services/imageRecognition/index.js
+++ services/imageRecognition/index.js
@@ -1,20 +1,18 @@
-// Imports the Google Cloud client library
 const vision = require('@google-cloud/vision');
 
-// Creates a client
 const client = new vision.ImageAnnotatorClient({
-    keyFilename: `${__dirname}/key.json`,
-  });
+  keyFilename: `${__dirname}/key.json`,
+});
 
 // Performs label detection on the image file
 client
   .labelDetection('./resources/cat.jpg')
-  .then(results => {
+  .then((results) => {
     const labels = results[0].labelAnnotations;
 
     console.log('Labels:');
     labels.forEach(label => console.log(label.description));
   })
-  .catch(err => {
+  .catch((err) => {
     console.error('ERROR:', err);
   });
