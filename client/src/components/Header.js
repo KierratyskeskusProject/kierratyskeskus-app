9feae5fc0a423c89923e1f8019d892624f92e47b
D00las <a1602665@myy.haaga-helia.fi>
2018-09-21 15:41:58 +0300

    Fixes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-    render() {
-        return (
-            <div className="header row">
-                <div className="logo col-3">
-                    <h1>Placeholder</h1>
-                </div>
-                <div className="headerContent col-5">
-                </div>
-                <div className="heading col-4">
-                    <h1>Add Item</h1>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="header row">
+        <div className="logo col-3">
+          <h1>Placeholder</h1>
+        </div>
+        <div className="headerContent col-5" />
+        <div className="heading col-4">
+          <h1>Add Item</h1>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Header;
