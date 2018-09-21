1e10acdfa331b60cb41da29518ea6f294f2f57d2
Severi Tikkanen <severitikkanen@gmail.com>
2018-09-21 11:13:48 +0300

    Added description as a seperate textarea Field

--- client/src/components/AddItemForm.js
+++ client/src/components/AddItemForm.js
@@ -10,17 +10,24 @@ import { postForm } from '../redux/actions/index';
 class AddItemForm extends Component {
   Submit(values) {
     postForm(values);
+  renderDescriptionField(field) {
+    return (
+      <div className="form-group">
+        <label>Product Description</label>
+        <textarea className="form-control" rows="3" {...field.input} />
+      </div>
+    );
   }
 
   renderFields() {
     return _.map(Fields, ({ label, name }) => (
-        <Field
-          key={name}
-          component={InputComponent}
-          type="text"
-          label={label}
-          name={name}
-        />
+      <Field
+        key={name}
+        component={InputComponent}
+        type="text"
+        label={label}
+        name={name}
+      />
     ));
   }
 
@@ -30,6 +37,11 @@ class AddItemForm extends Component {
       <div className="form-group">
         <form onSubmit={handleSubmit}>
           {this.renderFields()}
+          <Field
+            key="description"
+            name="description"
+            component={this.renderDescriptionField}
+          />
           <button className="btn btn-primary" type="submit">Submit</button>
         </form>
       </div>
