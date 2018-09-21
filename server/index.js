63ff352c3ab57704e459ae0cdae738d383415461
Iiro Niemi <iiro.niemi@gmail.com>
2018-09-21 14:09:10 +0300

    simple post endpoint

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const ProductRoutes = require('./routes/products.routes');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
   console.log(`Server is up on ${PORT}`);
