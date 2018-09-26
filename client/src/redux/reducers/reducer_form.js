e2b2fb4e3e6093c4f423a5ec1eacee677f2232a0
lateleinonen <a1604670@myy.haaga-helia.fi>
2018-09-26 14:41:03 +0300

    lint fixes

--- client/src/redux/reducers/reducer_form.js
+++ client/src/redux/reducers/reducer_form.js
@@ -3,7 +3,6 @@ import { POST_FORM } from '../actions/index';
 export default function (state = {}, action) {
   switch (action.type) {
     case POST_FORM:
-      break;
     default:
       return state;
   }
