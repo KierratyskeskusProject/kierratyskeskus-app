e63f35dad86bed3aaede01ebc32664507414f851
Severi Tikkanen <severitikkanen@gmail.com>
2018-09-26 16:31:16 +0300

    lint fixies

--- client/src/redux/reducers/reducer_form.js
+++ client/src/redux/reducers/reducer_form.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import { POST_FORM } from '../actions/index';
 
 // export default function (state = {}, action) {
@@ -8,14 +7,3 @@
 //       return state;
 //   }
 // }
-=======
-import { POST_FORM } from '../actions/index';
-
-export default function (state = {}, action) {
-  switch (action.type) {
-    case POST_FORM:
-    default:
-      return state;
-  }
-}
->>>>>>> e2b2fb4e3e6093c4f423a5ec1eacee677f2232a0
