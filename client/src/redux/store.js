33ef064fc7ba91c4f6e4e5377f7a949a313baca0
danni <danni.r.olsen@gmail.com>
2018-10-01 14:38:24 +0300

    redux store clean up

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 //import thunk from 'redux-thunk'; // middleware
 import rootReducer from './reducers/RootReducer';
-//import promise from 'redux-promise';
 
 export default function configureStore(initialState = {}) {
   return createStore(
     rootReducer,
-    //applyMiddleware(promise)
+    applyMiddleware()
    // get dev tool. https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd?hl=en
    + window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), // enables the redux dev tool in browser.
   );
