de6a8654d86dbfcd77e12eb8ab40f122f1976a17
danni <danni.r.olsen@gmail.com>
2018-10-02 11:40:00 +0300

    generate unique image name

--- server/controllers/images.controller.js
+++ server/controllers/images.controller.js
@@ -11,7 +11,7 @@ const Capture = (res) => {
 
   const dir = `${__dirname}/images/`;
   //const newName = Math.random(40).toString(36).replace(/[^a-z]+/g, '');
-    const newName = Date.now() + Math.random();
+  const newName = Date.now() + Math.random();
   const image = dir+newName+'.jpg';
 
   // to take picture from external web cam add name of device  as parameter to nodeWebcam.create({})
