2308d02f70430f092577e29b8044367194b5b6ba
Severi Tikkanen <severitikkanen@gmail.com>
2018-10-10 15:51:01 +0300

    Added appended button to product weight input field without styling

--- client/src/components/InputComponent.js
+++ client/src/components/InputComponent.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import UpdateWeightButton from './UpdateWeightButton';
 
 export default ({ input, label, meta }) => (
-  <div className="form-group">
+  <div className={`form-group ${label === 'Product weight' ? 'input-group' : ''}`}>
     <label>{label}</label>
     <input {...input} type="text" className={`form-control ${meta.touched && meta.error ? 'is-invalid' : ''}`} />
+    {label === 'Product weight' ? <UpdateWeightButton /> : null}
     <div className="invalid-feedback">{meta.touched ? meta.error : ''}</div>
   </div>
 );
