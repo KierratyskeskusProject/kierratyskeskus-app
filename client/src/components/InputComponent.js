e7d5558f00e11392e94106e46cb7b5ed5192f57e
Severi Tikkanen <severitikkanen@gmail.com>
2018-10-10 18:30:10 +0300

    Chnged bootstrap logic

--- client/src/components/InputComponent.js
+++ client/src/components/InputComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import UpdateWeightButton from './UpdateWeightButton';
 
 export default ({ input, label, meta }) => (
-  <div className={`form-group ${label === 'Product weight' ? 'input-group' : ''}`}>
+  <div className={`${label === 'Product weight' ? 'input-group' : 'form-group '}`}>
     <label>{label}</label>
     <input {...input} type="text" className={`form-control ${meta.touched && meta.error ? 'is-invalid' : ''}`} />
     {label === 'Product weight' ? <UpdateWeightButton /> : null}
