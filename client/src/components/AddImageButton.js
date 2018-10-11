4d787bc5ea1e4806e38ee4ddae8cdf029be166c3
IO89 <osipov.ivan.89@gmail.com>
2018-10-11 11:37:21 +0300

    clean up from dead code

--- client/src/components/AddImageButton.js
+++ client/src/components/AddImageButton.js
@@ -1,17 +1,4 @@
 import React from 'react';
-/*
-class ImageButton extends Component {
-
-
-  render() {
-    return (
-      <button className="btn addImage" type="button" onClick={()=> this.props.action}>
-        <i className="fa fa-camera" aria-hidden="true" />
-      </button>
-    );
-  }
-}
-*/
 
 const ImageButton = (props) => {
   const { action } = props;
