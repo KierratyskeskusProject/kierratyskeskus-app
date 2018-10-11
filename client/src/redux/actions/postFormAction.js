3157551cfaabe6aae15d9a0cd5158c0ce85c13ec
IO89 <osipov.ivan.89@gmail.com>
2018-10-11 11:37:21 +0300

    clean up from dead code

--- client/src/redux/actions/postFormAction.js
+++ client/src/redux/actions/postFormAction.js
@@ -3,7 +3,6 @@ import { postFormSuccess } from '../types';
 
 const postForm = (values) => {
   const request = axios.post('http://localhost:5000/products', values);
-
   return postFormSuccess(request);
 };
 
