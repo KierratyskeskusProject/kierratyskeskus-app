be99d14b96fe6c4f81df03e9640d198f32c3e4a5
Severi Tikkanen <severitikkanen@gmail.com>
2018-10-12 11:37:12 +0300

    Updated the button position to be inside input

--- client/src/components/InputComponent.js
+++ client/src/components/InputComponent.js
@@ -2,14 +2,16 @@ import React from 'react';
 import UpdateWeightButton from './UpdateWeightButton';
 
 export default ({ input, label, meta }) => (
-  <div className=/* {`${label === 'Product weight' ? 'input-group' : */"form-group"/* }`} */>
-    <label>{label}</label>
-    <input
-      {...input}
-      type="text"
-      className={`form-control ${meta.touched && meta.error ? 'is-invalid' : ''}`}
-    />
-    {label === 'Product weight' ? <UpdateWeightButton /> : null}
-    <div className="invalid-feedback">{meta.touched ? meta.error : ''}</div>
+  <div>
+    {<label>{label}</label>}
+    <div className={`${label === 'Product weight' ? 'input-group' : 'form-group'}`}>
+      <input
+        {...input}
+        type="text"
+        className={`form-control ${meta.touched && meta.error ? 'is-invalid' : ''}`}
+      />
+      {label === 'Product weight' ? <UpdateWeightButton /> : null}
+      <div className="invalid-feedback">{meta.touched ? meta.error : ''}</div>
+    </div>
   </div>
 );
