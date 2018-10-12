00b19bf711e9d1af5d7a02b7c0242c44cb0926a5
Severi Tikkanen <severitikkanen@gmail.com>
2018-10-12 09:48:19 +0300

    eslint line-brakes now not giving errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,11 +10,12 @@ module.exports = {
         "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
         "react/prefer-stateless-function": "off",
         "import/no-unresolved": "off",
-        "react/prop-types":"off",
-        "jsx-a11y/label-has-associated-control":"off",
-        "jsx-a11y/label-has-for":"off",
+        "react/prop-types": "off",
+        "jsx-a11y/label-has-associated-control": "off",
+        "jsx-a11y/label-has-for": "off",
         "class-methods-use-this": "off",
-        "no-console": 0
+        "no-console": 0,
+        "linebreak-style": 0
     },
     "plugins": [
         "react", "import"
