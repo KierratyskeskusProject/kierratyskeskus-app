9631f409d32437c2f731a413d633daf33e8ff842
IO89 <osipov.ivan.89@gmail.com>
2018-10-20 15:27:37 +0300

    travis config test

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const ProductRoutes = require('./routes/products.routes');
 const ImagesRoutes = require('./routes/images.routes');
 
@@ -18,6 +19,14 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+
+if (['ci'].includes(process.env.NODE_ENV)) {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
+  });
+}
+
 
 app.listen(PORT, () => {
   console.log(`Server is up on ${PORT}`);
