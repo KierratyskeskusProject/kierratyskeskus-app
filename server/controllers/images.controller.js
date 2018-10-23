d813ed9c8df67d80a9af632907aee14a649b9c6a
IO89 <osipov.ivan.89@gmail.com>
2018-10-23 13:51:01 +0300

    Merge branch 'cameraFix' into updateWeight
    # Conflicts:
    #	.travis.yml

--- server/controllers/images.controller.js
+++ server/controllers/images.controller.js
@@ -60,10 +60,8 @@ const Capture = (res) => {
           collection.imageInBase64 = imageToBase64(image);
           collection.labels = labelsArray;
           res.send(collection);
-          clearInterval(this.timer);
         })
         .catch((error) => {
-          clearInterval(this.timer);
           console.log(error);
           res.send(error);
         });
