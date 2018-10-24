7aa1ad384123cadbb90e92eab009232886dc7546
D00las <a1602665@myy.haaga-helia.fi>
2018-10-24 12:12:26 +0300

    Temporary fix to landscape images

--- client/src/components/Image.js
+++ client/src/components/Image.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const Image = (props) => {
   const { src } = props;
-  console.log('Toimiiko tämäkin?');
   return (
     <div className="newImage">
       <img src={src} alt="Loading unsuccessful" className="image" />
