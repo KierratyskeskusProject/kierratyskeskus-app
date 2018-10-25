d08d18c07bced0b2ba4f5e8dc8d4cb50926ab271
danni <danni.r.olsen@gmail.com>
2018-10-25 10:07:55 +0300

    initial scale setup

--- server/controllers/weight.controller.js
+++ server/controllers/weight.controller.js
@@ -2,55 +2,53 @@ const HID = require('node-hid');
 const usb = require('usb');
 
 const DymoScale = function dymoScale() {
-  const vid = 0x922;
-  const pid = 0x8003;
+  const vid = 0x0922; // 0x922 dymo 10kg
+  const pid = 0x8009; // 0x8003 dymo 10 kg
   let msg = '';
-  let attached = false;
 
-  /* const devicess = usb.findByIds(this.vid, this.pid);
-  console.log('DEVICES', devicess);
-  if (devicess.length > 0) {
-    this.deviceHandle = new HID.HID(devicess[0].path);
-    console.log(this.deviceHandle);
-  } else {
-    console.log(no)
-  } */
 
   usb.on('attach', (devicex) => {
     if (devicex.deviceDescriptor.idVendor === vid && devicex.deviceDescriptor.idProduct === pid) {
-      msg = 'Dymo M10 attached';
+      msg = 'scale ON';
       console.log(msg);
 
       this.weight = 0;
       this.error = null;
-      if (attached === true) {
-        this.deviceHandle = new HID.HID(devices[0].path);
-        this.device = (function findDevice() {
+
+
+      this.device = (function findDevice() {
+        if (!this.deviceHandle) {
+          console.log('running');
           const devices = HID.devices().filter(x => x.manufacturer === 'DYMO');
-          
-          return this.deviceHandle;
-        }());
-  
-        this.device.on('data', ([,,,, weight]) => {
-          this.weight = weight;
-          console.log(this.weight);
-        });
-  
-        this.device.on('error', (error) => {
-          this.error = error;
-        });
-  
-        this.read = function callbackFunc(callback) {
-          callback(this.error, { value: this.weight });
-        };
+
+          if (devices.length > 0) {
+            console.log('length');
+            this.deviceHandle = new HID.HID(devices[0].path);
+          }
+        }
+        return this.deviceHandle;
+      }());
+
+      this.device.on('data', ([,,,, weight]) => {
+        this.weight = weight;
+        console.log(this.weight);
       });
-      }
-      
+
+      this.device.on('error', (error) => {
+        console.log(error);
+        this.error = error;
+      });
+
+      this.read = function callbackFunc(callback) {
+        callback(this.error, { value: this.weight });
+      };
+    } // if devicex
+  });// usb on
+
   usb.on('detach', (devicex) => {
     if (devicex.deviceDescriptor.idVendor === vid && devicex.deviceDescriptor.idProduct === pid) {
-      msg = 'Dymo M10 detached';
+      msg = 'Scale OFF';
       console.log(msg);
-      attached = true;
     }
   });
 };
