d411cd139e55e370be88b33f3f2ea97af830abe3
danni <danni.r.olsen@gmail.com>
2018-10-25 11:40:27 +0300

    scale with issues

--- server/controllers/weight.controller.js
+++ server/controllers/weight.controller.js
@@ -1,43 +1,49 @@
 const HID = require('node-hid');
 const usb = require('usb');
+const events = require('events');
 
 const DymoScale = function dymoScale() {
   const vid = 0x0922; // 0x922 dymo 10kg
   const pid = 0x8009; // 0x8003 dymo 10 kg
   let msg = '';
+  let path = null;
+
 
 
   usb.on('attach', (devicex) => {
+
     if (devicex.deviceDescriptor.idVendor === vid && devicex.deviceDescriptor.idProduct === pid) {
       msg = 'scale ON';
       console.log(msg);
+      console.log('--------------------------------------------');
 
       this.weight = 0;
       this.error = null;
 
-
-      this.device = (function findDevice() {
-        if (!this.deviceHandle) {
-          console.log('running');
+      const findDevice = () => {
+        if(!this.deviceHandle) {
+          console.log('! this.deviceHandle');
           const devices = HID.devices().filter(x => x.manufacturer === 'DYMO');
-
-          if (devices.length > 0) {
-            console.log('length');
-            this.deviceHandle = new HID.HID(devices[0].path);
+          console.log(devices.length);
+          if(devices.length > 0) {
+            path = devices[0].path;
+            this.deviceHandle = new HID.HID(path);
+            path = null;
           }
         }
         return this.deviceHandle;
-      }());
+      }
 
-      this.device.on('data', ([,,,, weight]) => {
+      findDevice().on('data', ([,,,, weight]) => {
         this.weight = weight;
         console.log(this.weight);
       });
 
-      this.device.on('error', (error) => {
+      findDevice().on('error', (error) => {
         console.log(error);
         this.error = error;
       });
+
 
       this.read = function callbackFunc(callback) {
         callback(this.error, { value: this.weight });
