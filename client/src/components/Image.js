b1e2f77c1bdf225c13de4681254da975a5ac0ad6
D00las <a1602665@myy.haaga-helia.fi>
2018-10-26 13:54:04 +0300

    Working modals without styles or animations. WIP!!!

--- client/src/components/Image.js
+++ client/src/components/Image.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Modal } from 'react-bootstrap';
 
-const Image = (props) => {
-  const { src } = props;
-  return (
-    <div className="newImage">
-      <img src={src} alt="Loading unsuccessful" className="image" />
-    </div>
-  );
-};
+class Image extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      show: false,
+    };
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
+  render() {
+    const { src } = this.props;
+    const { show } = this.state;
+    return (
+      <div>
+        <button className="btn newImage" type="button" onClick={this.handleShow}>
+          <img src={src} alt="Loading unsuccessful" className="image" />
+        </button>
+        <Modal show={show} onHide={this.handleClose} animation={false}>
+          <Modal.Body>
+            <img src={src} alt="Loading unsuccessful" />
+            <p>I am here</p>
+          </Modal.Body>
+        </Modal>
+      </div>
+    );
+  }
+}
+
 export default Image;
