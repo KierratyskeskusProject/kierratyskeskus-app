8050fb92683b456e550181905e067e1ff2c0cfb2
IO89 <osipov.ivan.89@gmail.com>
2018-10-29 09:44:20 +0200

    Refactor of Images

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const DeleteButton = (props) => {
   const { action } = props;
-  console.log('delete button componet', props);
   return (
-    <button className="btn-deleteImage" type="button" onClick={action} />
+    <button className="btn-deleteImage" type="button" onClick={action}>
+      Delete
+    </button>
   );
 };
 export default DeleteButton;
