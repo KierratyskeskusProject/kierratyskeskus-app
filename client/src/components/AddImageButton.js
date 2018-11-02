5d0563e7a331b73e8c4adc1d2f5a039bbcbb779c
tarmeli <tommi.tarkki@gmail.com>
2018-11-02 14:13:13 +0200

    preview works and styling proposal

--- client/src/components/AddImageButton.js
+++ client/src/components/AddImageButton.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Webcam from 'react-webcam';
 
 const ImageButton = (props) => {
   const { action } = props;
   return (
     <button className="btn addImage" type="button" onClick={action}>
-      <i className="fa fa-camera" aria-hidden="true" />
+      <div className="camera__container">
+        <div className="camera__actual">
+          <Webcam height="200" />
+          <i className="fa fa-camera" aria-hidden="true" />
+        </div>
+      </div>
     </button>
   );
 };
