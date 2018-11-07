1c7c66fdfa55ba4c28d03cf8ecc351fc226cec7c
tarmeli <tommi.tarkki@gmail.com>
2018-11-07 14:27:33 +0200

    modal works, modal camera not so much

--- client/src/components/AddImageButton.js
+++ client/src/components/AddImageButton.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import { Modal } from 'react-bootstrap';
 import Webcam from 'react-webcam';
 
-const ImageButton = (props) => {
-  const { action } = props;
-  return (
-    <button className="btn addImage" type="button" onClick={action}>
-      <div className="camera__container">
-        <div className="camera__actual">
-          <Webcam height="200" />
-        </div>
-      </div>
-      <i className="fa fa-camera" aria-hidden="true" />
-    </button>
-  );
-};
+class ImageButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalActive: false,
+    };
+  }
+
+  handleModalToggle = () => {
+    const { isModalActive } = this.state;
+    this.setState({
+      isModalActive: !isModalActive,
+    });
+  }
+
+  render() {
+    const { isModalActive } = this.state;
+    const { handleModalToggle } = this;
+    return (
+      <Fragment>
+        <button className="btn addImage" type="button" onClick={handleModalToggle}>
+          <div className="camera__container">
+            <div className="camera__actual">
+              <Webcam height="200" />
+            </div>
+          </div>
+          <i className="fa fa-camera" aria-hidden="true" />
+        </button>
+        <Modal
+          show={isModalActive}
+          onHide={handleModalToggle}
+          animation={false}
+        >
+          <Modal.Body>
+            <Webcam />
+          </Modal.Body>
+        </Modal>
+      </Fragment>
+    );
+  }
+}
 export default ImageButton;
