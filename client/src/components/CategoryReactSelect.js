f597f958ca996702ad368a3ae0f22aaad27448bb
Severi Tikkanen <severitikkanen@gmail.com>
2018-11-23 09:45:35 +0200

    Removed old categories

--- client/src/components/CategoryReactSelect.js
+++ client/src/components/CategoryReactSelect.js
@@ -1,41 +1,6 @@
 import React, { Fragment } from 'react';
 import Select from 'react-select';
 
-/* const categoryList = [
-  {
-    label: 'Kitchenware',
-    value: 1,
-    options: [{
-      label: 'Plate',
-      value: 1.1,
-    }],
-  },
-  {
-    label: 'Bicycles',
-    value: 2,
-    options: [{
-      label: 'Wheel',
-      value: 2.1,
-    }],
-  },
-  {
-    label: 'Books',
-    value: 3,
-    options: [{
-      label: 'History',
-      value: 3.1,
-    },
-    {
-      label: 'Fantasy',
-      value: 3.2,
-    },
-    ],
-  },
-  { label: 'Electronics', value: 4 },
-  { label: 'Furniture', value: 5 },
-  { label: 'Clothes', value: 6 },
-]; */
-
 const CategoryReactSelect = (props) => {
   const { options, label, input: { value } } = props;
   return (
