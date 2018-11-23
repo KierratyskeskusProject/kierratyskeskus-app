601f1ef93afb8d5937fc90523a5039d7af3c6e7b
Severi Tikkanen <severitikkanen@gmail.com>
2018-11-23 10:42:50 +0200

    Merged validation to to this branch

--- client/src/components/CategoryReactSelect.js
+++ client/src/components/CategoryReactSelect.js
@@ -2,19 +2,29 @@ import React, { Fragment } from 'react';
 import Select from 'react-select';
 
 const CategoryReactSelect = (props) => {
-  const { options, label, input: { value } } = props;
+  const {
+    options, label, input: { value },
+  } = props;
+  console.log('Category', props);
+
+  function onInputChange(valueToChange) {
+    console.log('kekke');
+    return props.input.onChange(valueToChange);
+  }
+
   return (
     <Fragment>
       <label>{label}</label>
       <Select
         {...props}
         value={value}
-        onChange={valueToChange => props.input.onChange(valueToChange)}
+        onChange={onInputChange}
         onBlur={() => props.input.onBlur(props.input.value)}
         options={options}
         placeholder="Select a category"
         isMulti
         blurInputOnSelect={false}
+        classNamePrefix="form-control"
       />
     </Fragment>
   );
