9068142543df4037d94245ef39e7d2117532bec3
danni <danni.r.olsen@gmail.com>
2018-11-29 14:31:17 +0200

    find single template by subcategody id

--- server/routes/templates.routes.js
+++ server/routes/templates.routes.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const Template = require('../controllers/templates.controller');
 
 const TemplateRoutes = (app) => {
   // all templates
@@ -15,7 +14,8 @@ const TemplateRoutes = (app) => {
   });
 
   // template by id
-  app.get('/template/:id', (req, res) => {
+  // return default template if id doesen't exist
+  app.get('/templateById/:id', (req, res) => {
     const { id } = req.params;
 
     fs.readFile(`${__dirname}/data.json`, (err, data) => {
@@ -39,20 +39,55 @@ const TemplateRoutes = (app) => {
   });
 
 
-  // templates by category id
-  app.get('/templates/:catId', (req, res) => {
+  // All templates with category id
+  // return default template if id doesen't exist
+  app.get('/templatesByCategory/:catId', (req, res) => {
     const { catId } = req.params;
-    Template.getAllBySubCatId((catId), (err, templatesCat) => {
+    const templateRes = [];
+    fs.readFile(`${__dirname}/data.json`, (err, data) => {
       if (err) {
-        console.log(err);
-        return res.send(err);
-      }
-      res.status(200).send(templatesCat);
+        res.status(500).send('something went wrong');
+      } else {
+        const templates = JSON.parse(data);
+        const needle = catId;
 
-      return null;
+        for (let i = 0; i < templates.length; i++) {
+          console.log(templates[i].category);
+          console.log(needle.toString());
+          if (templates[i].category === needle) {
+            templateRes.push(templates[i]);
+          }
+        }
+      }
+      res.status(200).send(templateRes);
     });
   });
 
+
+  // template by sub_category id
+  app.get('/templateBySubCategory/:subCatId', (req, res) => {
+    const { subCatId } = req.params;
+
+    fs.readFile(`${__dirname}/data.json`, (err, data) => {
+      if (err) {
+        res.status(200).send('something went wrong');
+      } else {
+        const template = JSON.parse(data);
+        const needle = subCatId.toString();
+        let result = template[0];
+
+        for (let i = 0; i < template.length; i++) {
+          console.log('needle- ', needle);
+          console.log('sub cat ', template[i].sub_category);
+          if (template[i].sub_category === needle) {
+            result = template[i];
+            i = template.length;
+          }
+        }
+        res.status(200).send(result);
+      }
+    });
+  });
 
   app.post('/createTemplate', (req, res) => {
     const { template } = req.body;
@@ -76,3 +111,14 @@ const TemplateRoutes = (app) => {
 };
 
 module.exports = TemplateRoutes;
+
+/*
+  endpoints:
+
+  allTemplates
+  templateById/:id
+  templatesByCategory/:catId takes an integer
+  templateBySubCategory/:subCatId takes a double eg. 1.2
+  createTemplate takes an object temp_id, name, content, category, subcategory
+
+*/
