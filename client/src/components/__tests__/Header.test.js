49d2f4c9cdd16477cdf31143b84999cad646140a
Severi Tikkanen <severitikkanen@gmail.com>
2018-11-30 13:36:31 +0200

    Fixed Header test log error

--- client/src/components/__tests__/Header.test.js
+++ client/src/components/__tests__/Header.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
 import Header from '../Header';
 
 describe('Header', () => {
-  const header = shallow(<Header />);
+  const header = shallow(
+    <MemoryRouter>
+      <Header />
+    </MemoryRouter>,
+  );
 
   it('renders properly', () => {
-    expect(header.find('h1').length).toEqual(1);
+    expect(header.find(Header).exists()).toEqual(true);
   });
 });
